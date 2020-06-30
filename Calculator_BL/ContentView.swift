08cffa4ebcaa3ba31477ed1c558c8f70cb1644d0
Brendan Lenzner <brendan2614@outlook.com>
2020-06-30 16:08:11 -0500

    Initial Commit

---
+++ Calculator_BL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator_BL
+//
+//  Created by Brendan Lenzner on 6/30/20.
+//  Copyright © 2020 Brendan Lenzner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Calculator_BLTests/Calculator_BLTests.swift
@@ -0,0 +1,34 @@
+//
+//  Calculator_BLTests.swift
+//  Calculator_BLTests
+//
+//  Created by Brendan Lenzner on 6/30/20.
+//  Copyright © 2020 Brendan Lenzner. All rights reserved.
+//
+
+import XCTest
+@testable import Calculator_BL
+
+class Calculator_BLTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
